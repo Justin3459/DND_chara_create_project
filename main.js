ecc38ef014333a79b07ba995f5d04946c91f513b
Justin <justin3459@hotmail.com>
2023-05-02 13:53:01 -0400

    Added data to .json file:Barbarian, bard,cleric,druid,fighter,monk details

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 console.log("HTML works")
 
-fetch("https://www.dnd5eapi.co/api/classes").then(resp => resp.json()).then(json => console.log(json))
-fetch("http://localhost:3000/classes").then(resp => resp.json()).then(json => console.log(json))
+// fetch("https://www.dnd5eapi.co/api/classes").then(resp => resp.json()).then(json => console.log(json))
+fetch("http://localhost:3000/classes").then(resp => resp.json()).then(classes => createContainer(classes)
+)
+
+function createContainer(classes){
+        
+    const container = document.querySelector("ul#dndClassList")
+classes.forEach(dndClass => {
+    //class, description, 
+    //create element h2 to hold class name 
+    //create element to create description of class 
+    //create element to list to hold name and des
+    // append list to container
+    // Stretch: add photos to classes
+    
+    
+    
+});}
