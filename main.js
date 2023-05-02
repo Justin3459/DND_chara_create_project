283e99996dcef78781cf610153c35715447c6884
Justin <justin3459@hotmail.com>
2023-05-02 09:54:09 -0400

    js is connected to api and json file and is returning

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log("class")
+console.log("HTML works")
+
+fetch("https://www.dnd5eapi.co/api/classes").then(resp => resp.json()).then(json => console.log(json))
+fetch("http://localhost:3000/classes").then(resp => resp.json()).then(json => console.log(json))
