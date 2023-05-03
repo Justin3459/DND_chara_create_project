8eb8b2ff365e18beacb025c39399d7e9598c1775
Justin <justin3459@hotmail.com>
2023-05-03 13:18:28 -0400

    created createCardElement. recieving net err from local host

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 console.log("HTML works")
 
 // fetch("https://www.dnd5eapi.co/api/classes").then(resp => resp.json()).then(json => console.log(json))
-fetch("http://localhost:3000/classes").then(resp => resp.json()).then(classes => createContainer(classes)
-)
+fetch("http://localhost:3000/classes").then(resp => resp.json()).then(classes => createCardElement(classes))
 
-function createContainer(classes){
-        
-    const container = document.querySelector("ul#dndClassList")
-classes.forEach(dndClass => {
+function createCardElement(classes){
+     //creating div
+    let cardFront= document.createElement("div")
+    cardFront.classList.add("classCard")
+
+    let h2 = document.createElement("h2")
+    h2.textContent=classes.classes //create class name for card
+
+    //insert image to card
+    let img = document.createElement("img")
+    img.src = classes.image
+
+    let p = document.createElement("p")
+    p.textContent = ""// insert desc
+
+    cardFront.append(p, img, h2)
+    document.getElementById("class-card").appendChild(cardFront)
+
+
+//     const container = document.querySelector("ul#dndClassList")
+// classes.forEach(dndClass => {
     //class, description, 
     //create element h2 to hold class name 
     //create element to create description of class 
@@ -17,4 +33,4 @@ classes.forEach(dndClass => {
     
     
     
-});}
+};
---
+++ projectIdeas.md
@@ -0,0 +1,4 @@
+[]add spell slots to classes
+[]mouse over to flip card to reveil list
+    [] list contains of levels. maybe by user input
+[] change spells known to spells preped for classes
