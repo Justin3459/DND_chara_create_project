b9abdabb1fa8a2890e961e8a9bdb51a2e476226c
Justin <justin3459@hotmail.com>
2023-05-06 10:10:35 -0400

    div function that creates a card div with a front and back div inside

--- main.js
+++ main.js
@@ -1,36 +1,71 @@
 console.log("HTML works")
 
 // fetch("https://www.dnd5eapi.co/api/classes").then(resp => resp.json()).then(json => console.log(json))
-fetch("http://localhost:3000/classes").then(resp => resp.json()).then(classes => createCardElement(classes))
+fetch("http://localhost:3000/classes")
+.then(resp => resp.json())
+.then(classes => createCardElement(classes))
+// .then(classes => console.log(classes))
+.catch(error => console.log("failed to get"))
 
 function createCardElement(classes){
-     //creating div
-    let cardFront= document.createElement("div")
-    cardFront.classList.add("classCard")
+    classes.forEach(element => {
+        let div = document.createElement("div")
+        createFandBDivs(element)
+        
+    });
 
-    let h2 = document.createElement("h2")
-    h2.textContent=classes.classes //create class name for card
-
-    //insert image to card
-    let img = document.createElement("img")
-    img.src = classes.image
-
-    let p = document.createElement("p")
-    p.textContent = ""// insert desc
-
-    cardFront.append(p, img, h2)
-    document.getElementById("class-card").appendChild(cardFront)
-
-
-//     const container = document.querySelector("ul#dndClassList")
-// classes.forEach(dndClass => {
-    //class, description, 
-    //create element h2 to hold class name 
-    //create element to create description of class 
-    //create element to list to hold name and des
-    // append list to container
-    // Stretch: add photos to classes
+    // console.log(classes)
     
     
-    
-};
+    //     const container = document.querySelector("ul#dndClassList")
+    // classes.forEach(dndClass => {
+        //class, description, 
+        //create element h2 to hold class name 
+        //create element to create description of class 
+        //create element to list to hold name and des
+            // append list to container
+            // Stretch: add photos to classes  
+            const card = document.getElementsByClassName('cardFront')
+            card.addEventListener('click', function (){
+                card.classList.toggle('is-flipped')}) 
+        };
+
+        function createFandBDivs(element){
+            //create card div
+            let card = document.createElement("div")
+            card.classList = "card"
+            
+            //create cardFront
+            let cardFront = document.createElement("div")
+            cardFront.classList = "cardFront"
+            //create p in card front
+            let desFront = document.createElement("p")
+            desFront.textContent = element.description
+            cardFront.append(desFront)
+            //create classname in card Front as h2
+            let h2 = document.createElement("h2")
+            h2.id = "class-name"
+            h2.textContent=element.name
+            cardFront.append(h2)
+            //create img and append to h2
+            let img = document.createElement("img")
+            img.src = element.image
+            h2.append(img)
+            // cardFront.textContent = element.name
+            // cardFront.image = element.image
+        
+            //create cardBack
+            let cardBack = document.createElement("div")
+            cardBack.classList = "cardBack"
+            let desBack = document.createElement("p")
+            let levelArray = JSON.stringify(element.level)
+            desBack.textContent = levelArray
+            cardBack.append(desBack)
+
+            card.append(cardBack, cardFront)
+            document.getElementById("container").append(card)
+        }
+
+function cardFlip(){
+    card.classList.toggle('is-flipped')
+}
