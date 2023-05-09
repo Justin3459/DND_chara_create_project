f8a41dc2dc3fb475e7f8219d5094acd57430e80e
Justin <justin3459@hotmail.com>
2023-05-08 21:04:10 -0400

    made function to iterate over json called getJsonData. created a function called updateLevelInfo that uses forEach to iterate over the cards called returnDesDetail. created returnDesDetail that uses find to compare h2 of card to .name. cant populate anything on html again. also cleaned up comments and unused code

--- main.js
+++ main.js
@@ -1,109 +1,90 @@
-console.log("HTML works")
-
 // fetch("https://www.dnd5eapi.co/api/classes").then(resp => resp.json()).then(json => console.log(json))
 fetch("http://localhost:3000/classes")
 .then(resp => resp.json())
 .then(classes => createCardElement(classes))
-// .then(classes => console.log(classes))
 .catch(error => console.log("failed to get"))
-
+// .then(classes => console.log(classes, element))
+//when element arguement is passed the page does not load
 function createCardElement(classes){
     classes.forEach(element => {
         createFandBDivs(element)
     });
-    
+function createFandBDivs(element){
+    let card = document.createElement("div")
+    card.classList = "card"
 
-    // console.log(classes)
-    
-    
-    //     const container = document.querySelector("ul#dndClassList")
-    // classes.forEach(dndClass => {
-        //class, description, 
-        //create element h2 to hold class name 
-        //create element to create description of class 
-        //create element to list to hold name and des
-            // append list to container
-            // Stretch: add photos to classes  
-            cardFlip()
-        };
+    let cardFront = document.createElement("div")
+    cardFront.classList = "cardFront"
+
+    let desFront = document.createElement("p")
+    desFront.textContent = element.description
+    cardFront.append(desFront)
+
+    let h2 = document.createElement("h2")
+    h2.id = "class-name"
+    h2.textContent=element.name
+    cardFront.append(h2)
+
+    let img = document.createElement("img")
+    img.src = element.image
+    h2.append(img)
+
+    let cardBack = document.createElement("div")
+    cardBack.classList = "cardBack"
+
+    let desBack = document.createElement("p")
+    // console.log(desBackInfo)
+    //need to create function to get information on the back
+    desBack.textContent = getJsonData(element, levelSelect.value)
+    cardBack.append(desBack)
+
+    card.append(cardBack, cardFront)
+    document.getElementById("container")
+        .append(card)
+}
+
+    cardFlip()
 
 function cardFlip(){
-    const cards = document.querySelectorAll('.card');
+    const cards = document.querySelectorAll('.card')
     cards.forEach(function(card) {
       card.addEventListener('click', function() {
         this.classList.toggle('flipped');
       });
     });
 }
-function createFandBDivs(element){
-        //create card div
-            let card = document.createElement("div")
-            card.classList = "card"
-            
-        //create cardFront
-            let cardFront = document.createElement("div")
-            cardFront.classList = "cardFront"
-        //create p in card front
-            let desFront = document.createElement("p")
-            desFront.textContent = element.description
-            cardFront.append(desFront)
-        //create classname in card Front as h2
-            let h2 = document.createElement("h2")
-            h2.id = "class-name"
-            h2.textContent=element.name
-            cardFront.append(h2)
-        //create img and append to h2
-            let img = document.createElement("img")
-            img.src = element.image
-            h2.append(img)
-            // cardFront.textContent = element.name
-            // cardFront.image = element.image
+};
 
-            //create cardBack
-            let cardBack = document.createElement("div")
-            cardBack.classList = "cardBack"
-
-            let desBack = document.createElement("p")
-            const desBackInfo = json.stringify(element)
-            console.log(desBackInfo)
-            desBack.textContent = "desBackInfo"
-            cardBack.append(desBack)
-
-            //return just barbarianinfo
-            // const barbarianInfo = element.level[0]
-            // const barbarianInfoHTML = barbarianInfo.features
-            //     .map(feature =>{`<li>${feature.name}: ${feature.description}</li>`})
-            //     .join('')
-            // cardBack.append(barbarianInfoHTML)
-            //display level information appended to back. 
-                //how to extract just level1
-                //use value of dropdown menu?
-            const levelSelect = document.getElementById("level-select")
-            //dropdown menu eventlistener=>change
-        //     levelSelect.addEventListener("change", updateLevelInfo)
-            card.append(cardBack, cardFront)
-            document.getElementById("container").append(card)
-        //function to create cardBack information
+function getJsonData(obj, element){
+    let jsonString = JSON.stringify(obj)
+    let jsonParse = JSON.parse(jsonString)
+    let levelData = jsonParse.level[0][element] 
+    //need to iterate through nested object
+    if (typeof levelData === "object"){
+    console.log(levelData)
+        return JSON.stringify(leveldata)
     }
+    else {
+        return levelData
+    }  
+}   
 
-    //     function updateLevelInfo(){
-        //         const selectedLevel = levelSelect.value;
-        //         const levelInfo = element.level[0][selectedLevel]
-                
-                
-                
-        //         // const selectedLevel = parseInt(levelSelect.value)
-        //         // const levelData = element.level[selectedLevel - 1]
-        //         // cardBack.append(levelData)
-        //     }
-        //     
-
-        // }
-        function displayLevelInfo(level) {
-            const levelData = element.level[level.toString()];
-            const levelInfoHTML = Object.values(levelData[0]).map((info) => {
-              return `<li>${info}</li>`;
-            });
-            const levelListHTML = `<ul>${levelInfoHTML.join('')}</ul>`;
-            document.getElementById('cardBack').append(levelListHTML)
-        }
+const levelSelect = document.getElementById("level-select")
+levelSelect.addEventListener("change", (e) => updateLevelInfo(e))
+//function to get level number from drop down
+function updateLevelInfo(e){
+    const selectedLevel = e.target.value
+    console.log(selectedLevel)
+    const cards = document.querySelectorAll('.card')
+    //iterate through classes
+    cards.forEach(returnDesDetail(element))
+    const selectedObj = document.getElementById("class-select").value
+}
+//how to get nested object informantion. **
+function returnDesDetail(element){
+    let desBack = element.querySelector('.cardBack p')
+    let className = element.querySelector('.cardFront h2').textContent
+    //iterate through json to make a string then 
+    desBack.textContent = getJsonData(classes.find(c => c.name === className), selectedLevel)
+}
+ 
--- projectIdeas.md
+++ projectIdeas.md
@@ -2,3 +2,13 @@
 []mouse over to flip card to reveil list
     [] list contains of levels. maybe by user input
 [] change spells known to spells preped for classes
+
+    // console.log(classes)
+    //     const container = document.querySelector("ul#dndClassList")
+    // classes.forEach(dndClass => {
+        //class, description, 
+        //create element h2 to hold class name 
+        //create element to create description of class 
+        //create element to list to hold name and des
+            // append list to container
+            // Stretch: add photos to classes  
