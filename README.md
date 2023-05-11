4a6d10ed92e32e27ebc0f88df296d7c7b9ca61bd
Justin <justin3459@hotmail.com>
2023-05-11 10:02:37 -0400

    added to readMe and added variables to be used when building character sheet

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # DND_chara_create_project
+ The DND Previewer is a web app that allows the user to preview 12 different classes at various levels. This information is based off of DND 5e Player HandBook class section. This interactive application allows ther user to select a level and display information in the classes cards.
+
+ Features
+ -two sided cards:
+    -Front of the card is the class name, description, and image
+    -Back of the card displays what abilities or features the class gets at a selected level
+ -select a level from the dropdown menu at the top of the page  
+ -interactive card that rotates when clicked to display front and back of clicked card
+ -cards have a vertical scroll to display more text if needed
+
+ Languages used
+ -HTML 
+ -CSS
+ -Javascript
+
+ 
--- main.js
+++ main.js
@@ -1,4 +1,71 @@
 let classes;
+// let name = "user input character name"
+// let attributes ={
+//     strength : 0,
+//     dexterity : 0,
+//     constitution : 0,
+//     intelligence : 0,
+//     wisdom : 0,
+//     charisma : 0
+// }
+
+// let savingThrows = {
+//     strength: 0,
+//     dexterity:0,
+//     constitution:0,
+//     intelligence:0,
+//     wisdom:0,
+//     charisma:0
+// //modifiers are per 2 levels +-1 modifier
+// }
+
+// let skills = {
+//     acrobatics: "dex mod",
+//     animalHandling: "wis mod",
+//     arcana:"int mod",
+//     athletics: "str mod",
+//     deception: "cha mod",
+//     history: "int mod",
+//     insight: "wis mod",
+//     intimidation: "cha mod",
+//     investigation: "int mod",
+//     medicine: "wis mod",
+//     nature: "int mod",
+//     perception: "wis mod",
+//     performance: "cha mod",
+//     persuasion: "cha mod",
+//     religion: "int mod",
+//     sleightOfHand: "dex mod",
+//     stealth: "dex mod",
+//     survival: "wis mod"
+// }
+// let passivePerception = "8 + proficientcy + perception"
+// let armorClass = "10+dex or armor. maybe use user input unless i want to build a json with base items"
+// let initiative = "dex mod. use user input? unless add feat json"
+// let speed = "user input. unless json feat + race."
+//let currentHitPoints = "user input. class determined. classes Json needs to be modified"
+//let tempHitPoints = "user input"
+//let hitDice = "user input. classes Json needs to be modified"
+//let deathDice = "user input. not needed for character create?"
+//let melee = "user input or json of weapon, modifier, attack bonus, range?"
+//let range = "user input or json of weapon, modifier, attack bonus, range"
+//let spellCast = "user input. spells json?"
+//let gold = {cp:,sp:,gp:,ep:} "user input"
+//let equipment = "user input. equipment json"
+//let language = "user input. race, class, and background affected"
+//let feats = "user input. feat json"
+//let personalityTrait = "user input"
+//let ideals = "user input"
+//let bonds = "user input"
+//let flaws  = "user input"
+// let level = "user selected level from level-select"
+//let background = "user input? background json. backgrounds add some bonuses"
+//let faction = "user input"
+//let race = "user input. race json. races add bonuses"
+//let alignment = "user input. maybe dropdown select"
+//let experience = "user input"
+// 
+
 const levelSelect = document.getElementById("level-select")
 
 fetch("http://localhost:3000/classes")
@@ -43,6 +110,14 @@ function createFandBDivs(element){
     desBack.textContent = getJsonData(element, levelSelect.value)
     cardBack.append(desBack)
 
+    //add button to bottom of the card. cardFront and cardBack.
+    //button text is "create adventurer"
+    let button = document.createElement("button")
+    button.textContent = "Create Adventurer"
+    //add eventListener to push selected card to array/nest object
+    button.addEventListener('click' , )
+
+    cardBack.appendChild(button)
     card.append(cardBack, cardFront)
     document.getElementById("container")
         .append(card)
@@ -79,5 +154,7 @@ levelSelect.addEventListener("change", (e) => {
     })
 })
 
-    
+//desBack information should be displayed as a li 
+//do i need to contain it in a ul?
+//use levelSelect? to display each item in selectedLevel as an li then append to desBack 
 
