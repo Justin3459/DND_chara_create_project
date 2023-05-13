c3f0db1e919d21fd4fd70a184e177e119c2cda61
Justin <justin3459@hotmail.com>
2023-05-13 16:05:31 -0400

    comment out desBack forEach due to html not properly loading

--- main.js
+++ main.js
@@ -106,20 +106,17 @@ function createFandBDivs(element){
     let cardBack = document.createElement("div")
     cardBack.classList = "cardBack"
 
-    //create list. UL to LI?
     let ul = document.createElement('ul')
-    //append to ul.
-    //text content of li will be data from json. forEach?
-    //use getJsonData to get elements of level
+
     let desBack = getJsonData(element, levelSelect.value)
-    //use desBack with a forEach to get level data?
-    let li = document.createElement('li')
     desBack.forEach(function (desLevel){
+        let li = document.createElement('li')
         li.textContent = desLevel
         ul.appendChild(li)
     })
-    cardBack.append(ul)
-
+    
+    cardBack.appendChild(ul)
+    
     //add button to bottom of the card. cardFront and cardBack.
     //button text is "create adventurer"
     let button = document.createElement("button")
